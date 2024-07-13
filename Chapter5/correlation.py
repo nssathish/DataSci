413e1805b5b9fd0dc743cad9932e01a038adefca
Sathish Kumar Subramanian <nsubramanian.sathish@gmail.com>
2024-07-13 09:54:20 +0530

    simpsons paradox

--- Chapter5/correlation.py
+++ Chapter5/correlation.py
@@ -22,8 +22,14 @@ class CorrelationCausation:
         """Measures how much xs and ys varies in tandem about their means"""
         stdev_x = self.dv.standard_deviation(xs)
         stdev_y = self.dv.standard_deviation(ys)
+        print("standar deviations", (stdev_x, stdev_y))
 
-        correlation = self.covariance(xs, ys) / stdev_x / stdev_y
+        correlation = (
+            self.covariance(xs, ys) / (stdev_x * stdev_y)
+            if stdev_x > 0 and stdev_y > 0
+            else 0
+        )
+
         return correlation
 
 
@@ -71,3 +77,10 @@ daily_minutes_good = [minute for i, minute in enumerate(daily_minutes) if i != o
 daily_hours_good = [hour for i, hour in enumerate(daily_hours) if i != outlier]
 pprint(cc.correlation(num_friends_good, daily_minutes_good))
 pprint(cc.correlation(num_friends_good, daily_hours_good))
+
+x = [-2, -1, 0, 1, 2]
+y = [2, 1, 0, 1, 2]
+print("correlation between x and y", CorrelationCausation().correlation(x, y))
+
+y = [99.98, 99.99, 100, 100.01, 100.02]
+print("correlation between x and modified y", CorrelationCausation().correlation(x, y))
